0e48edf022701d8b40430b0716740f553caf0532
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-28 15:42:48 -0400

    Add user data structure, with storage

---
+++ src/data.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "io/ioutil"
+)
+
+type user struct {
+  Handle string
+}
+
+func userpath(handle string) string {
+  return DATA_PATH + "/" + handle + ".user"
+}
+
+func (this *user) save() error {
+  return ioutil.WriteFile(
+    userpath(this.Handle),
+    []byte(this.Handle),
+    600,
+  )
+}
+
+func loadUser(handle string) (*user, error) {
+  in, err := ioutil.ReadFile(userpath(handle))
+
+  if err != nil {
+    return nil, err
+  }
+
+  return &user{
+    Handle: string(in),
+  }, nil
+}
