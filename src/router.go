3a44218fa58f4570e89ee1b02f86ef195ba53598
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-29 11:38:21 -0400

    Separate session code

--- src/router.go
+++ src/router.go
@@ -5,8 +5,6 @@ import (
   "html/template"
   "log"
   "net/http"
-  "strings"
-  "time"
 )
 
 type router struct {
@@ -22,51 +20,6 @@ func newRouter() *router {
   this.mux.HandleFunc("/logout", logout)
 
   return this
-}
-
-func newSession(writer http.ResponseWriter, account *user) {
-  token := account.refreshToken()
-
-  session := http.Cookie{
-    Name:  SESSION_NAME,
-    Value: account.Handle + DELM + token,
-  }
-
-  http.SetCookie(writer, &session)
-  log.Printf("Created new session with token %s", token)
-}
-
-func clearSession(writer http.ResponseWriter) {
-  session := http.Cookie{
-    Name:    SESSION_NAME,
-    Value:   "",
-    Expires: time.Now().Add(-time.Minute),
-  }
-
-  http.SetCookie(writer, &session)
-  log.Printf("Cleared session")
-}
-
-func loadSession(request *http.Request) (*user, error) {
-  session, err := request.Cookie(SESSION_NAME)
-
-  if err != nil {
-    return nil, err
-  }
-
-  split := strings.Split(session.Value, DELM)
-  account, err := loadUser(split[0])
-
-  if err != nil {
-    return nil, err
-  }
-
-  if err = account.checkToken(split[1]); err != nil {
-    return nil, err
-  }
-
-  log.Printf("Loaded session with token %s", split[1])
-  return account, nil
 }
 
 func serveTemplate(
