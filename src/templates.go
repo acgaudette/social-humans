818abc89305ade0060cf0876fba2ecac653fad7b
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-29 18:31:32 -0400

    Add pool users template data structure

--- src/templates.go
+++ src/templates.go
@@ -9,6 +9,52 @@ type statusMessage struct {
 	Status string
 }
 
+type poolUsers struct {
+	Handles []string
+	Status  string
+}
+
+func getPoolUsers(handle string, status string) (*poolUsers, error) {
+	data, err := loadPool(handle)
+
+	if err != nil {
+		empty := &poolUsers{
+			Handles: []string{},
+			Status:  "Access failure",
+		}
+
+		return empty, err
+	}
+
+	if len(data.users) <= 1 {
+		if status == "" {
+			status = "Your pool is empty!"
+		}
+
+		empty := &poolUsers{
+			Handles: []string{},
+			Status:  status,
+		}
+
+		return empty, nil
+	}
+
+	result := &poolUsers{
+		Handles: []string{},
+		Status:  status,
+	}
+
+	for _, value := range data.users {
+		if value == handle {
+			continue
+		}
+
+		result.Handles = append(result.Handles, value)
+	}
+
+	return result, nil
+}
+
 func serveTemplate(
 	writer http.ResponseWriter, path string, data interface{},
 ) error {
