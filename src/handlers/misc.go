d3995f6b2bf4b250daec3a24f9b8b9a4cad4238b
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-30 15:13:58 -0400

    Add closure as parameter to form read function

--- src/handlers/misc.go
+++ src/handlers/misc.go
@@ -1,36 +1,26 @@
 package handlers
 
 import (
-	"../front"
 	"errors"
-	"log"
 	"net/http"
 	"strings"
 )
 
-func serveStatusError(template, status string, writer http.ResponseWriter) {
-	message := front.StatusMessage{Status: status}
-	err := front.ServeTemplate(writer, template, &message)
-
-	if err != nil {
-		front.Error501(writer)
-		log.Printf("%s", err)
-	}
-}
+type errorClosure func(string)
 
 func readFormString(
 	template, key, errorStatus string,
-	writer http.ResponseWriter, request *http.Request,
+	fail errorClosure, request *http.Request,
 ) (string, error) {
 	result := request.Form.Get(key)
 
 	if result == "" {
-		serveStatusError(template, errorStatus, writer)
+		fail(errorStatus)
 		return "", errors.New("key not found for string")
 	}
 
 	if strings.IndexRune(result, '+') >= 0 {
-		serveStatusError(template, "Invalid input", writer)
+		fail(errorStatus)
 		return "", errors.New("invalid input")
 	}
 
