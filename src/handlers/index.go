140b6cd916567bf4e2d5d39d6a70a8c99d3e8972
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-30 05:13:51 -0400

    Remove redundant path check

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -8,19 +8,15 @@ import (
 )
 
 func Index(writer http.ResponseWriter, request *http.Request) {
-	if request.URL.Path == "/" {
-		data, err := data.GetUserFromSession(request)
+	data, err := data.GetUserFromSession(request)
 
-		if err != nil {
-			log.Printf("%s", err)
-			http.Redirect(writer, request, "/login", http.StatusFound)
-			return
-		}
+	if err != nil {
+		log.Printf("%s", err)
+		http.Redirect(writer, request, "/login", http.StatusFound)
+		return
+	}
 
-		if err = front.ServeTemplate(writer, "/index.html", data); err != nil {
-			log.Printf("%s", err)
-		}
-	} else {
-		http.NotFound(writer, request)
+	if err = front.ServeTemplate(writer, "/index.html", data); err != nil {
+		log.Printf("%s", err)
 	}
 }
