82074641fd93f161a3d9bb909fc59ac3f7740965
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-30 11:10:10 -0400

    Preload nested templates

--- src/front/templates.go
+++ src/front/templates.go
@@ -1,33 +1,49 @@
 package front
 
 import (
+	"errors"
 	"html/template"
+	"log"
 	"net/http"
+	"path/filepath"
 )
 
-type StatusMessage struct {
-	Status string
-}
+type templateLookup map[string]*template.Template
 
-type UserView struct {
-	Handle string
-}
+var templates templateLookup
 
-type PoolView struct {
-	Handles []string
-	Status  string
+func init() {
+	if templates == nil {
+		templates = make(templateLookup)
+	}
+
+	files, err := filepath.Glob(ROOT + "/*.html")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range files {
+		base := filepath.Base(file)
+
+		if base == "layout.html" {
+			continue
+		}
+
+		targets := append([]string{ROOT + "/layout.html"}, file)
+		templates[base] = template.Must(template.ParseFiles(targets...))
+	}
 }
 
 func ServeTemplate(
 	writer http.ResponseWriter, path string, data interface{},
 ) error {
-	t, err := template.ParseFiles(ROOT + path)
+	target, ok := templates[path+".html"]
 
-	if err != nil {
-		return err
+	if !ok {
+		return errors.New("template does not exist")
 	}
 
-	err = t.Execute(writer, data)
-
-	return err
+	writer.Header().Set("Content-Type", "text/html; charset=utf-8")
+	return target.ExecuteTemplate(writer, "layout", data)
 }
