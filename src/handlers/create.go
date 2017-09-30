c96f1f67d9b637efa0fac9cdca73e9d6f26bcf54
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-30 14:34:36 -0400

    Factor out handler code into misc

--- src/handlers/create.go
+++ src/handlers/create.go
@@ -3,40 +3,9 @@ package handlers
 import (
 	"../data"
 	"../front"
-	"errors"
 	"log"
 	"net/http"
-	"strings"
 )
-
-func serveStatusError(template, status string, writer http.ResponseWriter) {
-	message := front.StatusMessage{Status: status}
-	err := front.ServeTemplate(writer, template, &message)
-
-	if err != nil {
-		front.Error501(writer)
-		log.Printf("%s", err)
-	}
-}
-
-func readFormString(
-	template, key, errorStatus string,
-	writer http.ResponseWriter, request *http.Request,
-) (string, error) {
-	result := request.Form.Get(key)
-
-	if result == "" {
-		serveStatusError(template, errorStatus, writer)
-		return "", errors.New("key not found for string")
-	}
-
-	if strings.IndexRune(result, '+') >= 0 {
-		serveStatusError(template, "Invalid input", writer)
-		return "", errors.New("invalid input")
-	}
-
-	return result, nil
-}
 
 func GetCreate(writer http.ResponseWriter, request *http.Request) {
 	err := front.ServeTemplate(
