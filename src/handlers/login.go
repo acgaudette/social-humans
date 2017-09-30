7180f1d24b540058806623511b358216a0b20d31
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-09-30 14:31:02 -0400

    Update login handler to new API

--- src/handlers/login.go
+++ src/handlers/login.go
@@ -3,15 +3,13 @@ package handlers
 import (
 	"../data"
 	"../front"
-	"errors"
 	"log"
 	"net/http"
-	"strings"
 )
 
 func GetLogin(writer http.ResponseWriter, request *http.Request) {
 	err := front.ServeTemplate(
-		writer, "login", &front.StatusMessage{Status: ""},
+		writer, "login", &front.StatusMessage{},
 	)
 
 	if err != nil {
@@ -23,40 +21,18 @@ func GetLogin(writer http.ResponseWriter, request *http.Request) {
 func Login(writer http.ResponseWriter, request *http.Request) {
 	request.ParseForm()
 
-	serveError := func(status string) {
-		message := front.StatusMessage{Status: status}
-		err := front.ServeTemplate(writer, "login", &message)
-
-		if err != nil {
-			front.Error501(writer)
-			log.Printf("%s", err)
-		}
-	}
-
-	readString := func(key string, errorStatus string) (string, error) {
-		result := request.Form.Get(key)
-
-		if result == "" {
-			serveError(errorStatus)
-			return "", errors.New("key not found for string")
-		}
-
-		return result, nil
-	}
-
-	handle, err := readString("handle", "Username required!")
+	handle, err := readFormString(
+		"login", "handle", "Username required!", writer, request,
+	)
 
 	if err != nil {
 		log.Printf("%s", err)
 		return
 	}
 
-	if strings.IndexRune(handle, '+') >= 0 {
-		serveError("Invalid username")
-		return
-	}
-
-	password, err := readString("password", "Password required!")
+	password, err := readFormString(
+		"login", "password", "Password required!", writer, request,
+	)
 
 	if err != nil {
 		log.Printf("%s", err)
@@ -66,15 +42,8 @@ func Login(writer http.ResponseWriter, request *http.Request) {
 	account, err := data.LoadUser(handle)
 
 	if err != nil {
-		log.Printf("%s", err)
-
-		account, err = data.AddUser(handle, password)
-
-		if err != nil {
-			log.Printf("%s", err)
-			front.Error501(writer)
-			return
-		}
+		serveStatusError("login", "User does not exist!", writer)
+		return
 	} else if err = account.Validate(password); err != nil {
 		log.Printf("%s", err)
 
