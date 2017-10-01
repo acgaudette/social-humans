06404a67c4923f0410670b977de2d9103f088675
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-01 14:09:58 -0400

    Return app error from handler, handle responses

---
+++ src/handler.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"./app"
+	"./front"
+	"log"
+	"net/http"
+)
+
+type Handler func(http.ResponseWriter, *http.Request) *app.Error
+
+func handle(handler Handler, out http.ResponseWriter, in *http.Request) {
+	// Execute handler
+	err := handler(out, in)
+
+	// Handle error responses
+	if err != nil {
+		switch err.Code {
+		case app.SERVER:
+			front.Error501(out)
+
+		case app.NOT_FOUND:
+			http.NotFound(out, in)
+
+		case app.REDIRECT:
+			// Redirect is taken care of in the handler
+		}
+
+		// Log error message
+		if err.Native != nil {
+			log.Printf("%s", err.Native)
+		}
+	}
+
+	// Otherwise, response was sent in the handler
+}
