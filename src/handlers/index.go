3dabfa14cb38728f9fa423edb9098d84b498e9c1
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 02:17:35 -0400

    Don't redirect from index if not logged in

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -12,11 +12,12 @@ func Index(out http.ResponseWriter, in *http.Request) *app.Error {
 	// Load current user, if available
 	account, err := data.GetUserFromSession(in)
 
-	// Redirect to login page if there is no session open
-	if err != nil {
-		return front.Redirect("/login", err, out, in)
+	// User exists, so serve with user view
+	if err == nil {
+		view := control.GetUserView(account, "", in)
+		return front.ServeTemplate(out, "index", view)
 	}
 
-	view := control.GetUserView(account, "", in)
-	return front.ServeTemplate(out, "index", view)
+	// Otherwise, serve plain template
+	return front.ServeTemplate(out, "index", nil)
 }
