135f3c43265b01f037d0f93c59d9e049b6e74173
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 02:54:44 -0400

    Store timestamp in post struct

--- src/data/post.go
+++ src/data/post.go
@@ -6,26 +6,28 @@ import (
 )
 
 type post struct {
-	title    string
-	content  string
-	author   string
-	metadata []string
+	title     string
+	content   string
+	author    string
+	timestamp string
 }
 
 func (this *post) save() error {
-	name := time.Now().UTC().Format(TIME_LAYOUT)+"_"+this.author
 	return ioutil.WriteFile(
-		path(name, "post"),
+		path(this.timestamp + "_" + this.author, "post"),
 		[]byte(this.content),
 		0600,
 	)
 }
 
 func NewPost(title, content, author string) error {
+	stamp := time.Now().UTC().Format(TIME_LAYOUT)
+
 	this := &post{
-		title: title,
-		content: content,
-		author: author,
+		title:     title,
+		content:   content,
+		author:    author,
+		timestamp: stamp,
 	}
 
 	return this.save()
