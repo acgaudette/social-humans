b768cfa3108b89070912ac80cc76a82ac9fe1856
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 02:39:17 -0400

    Add 501 wrapper function

--- src/front/errors.go
+++ src/front/errors.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 )
 
+// Redirect client and return error for router to handle
 func Redirect(
 	route string, err error, out http.ResponseWriter, in *http.Request,
 ) *app.Error {
@@ -13,5 +14,13 @@ func Redirect(
 	return &app.Error{
 		Native: err,
 		Code:   app.REDIRECT,
+	}
+}
+
+// Return server error (501) for router to handle
+func ServerError(err error) *app.Error {
+	return &app.Error{
+		Native: err,
+		Code:   app.SERVER,
 	}
 }
