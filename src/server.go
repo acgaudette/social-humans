a23f23ea2d5478bd7a3ae37ad8792a486d353550
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 02:50:45 -0400

    Split routes to separate file

--- src/server.go
+++ src/server.go
@@ -10,30 +10,7 @@ import (
 
 func newServer() *http.Server {
 	mux := NewRouter(handlers.Index)
-
-	mux.GET("/login", handlers.GetLogin)
-	mux.POST("/login", handlers.Login)
-
-	mux.GET("/logout", handlers.GetLogout)
-	mux.POST("/logout", handlers.Logout)
-
-	mux.GET("/create", handlers.GetCreate)
-	mux.POST("/create", handlers.Create)
-
-	mux.GET("/edit", handlers.GetEdit)
-	mux.POST("/edit", handlers.Edit)
-
-	mux.GET("/delete", handlers.GetDelete)
-	mux.POST("/delete", handlers.Delete)
-
-	mux.GET("/pool", handlers.GetPool)
-	mux.POST("/pool", handlers.ManagePool)
-
-	mux.GET("/me", handlers.Me)
-	mux.GET("/user/*", handlers.GetUser)
-
-	mux.GET("/post", handlers.GetPost)
-	mux.POST("/post", handlers.CreatePost)
+	addRoutes(mux)
 
 	return &http.Server{
 		Addr:    ADDRESS + ":" + PORT,
