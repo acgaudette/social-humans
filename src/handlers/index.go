783edf2b54100f14fd4fb16d696f8c266cdd969f
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 16:47:05 -0400

    Rewrite index handler for feed display

--- src/handlers/index.go
+++ src/handlers/index.go
@@ -3,11 +3,29 @@ package handlers
 import (
 	"../app"
 	"../control"
+	"../data"
 	"../front"
+	"log"
 	"net/http"
 )
 
 func Index(out http.ResponseWriter, in *http.Request) *app.Error {
-	views := control.GetUserAndMakeViews(control.MakeFeedView(), in)
+	// Load current user, if available
+	active, err := data.GetUserFromSession(in)
+
+	// Serve blank index if there is no session open
+	if err != nil {
+		views := control.MakeViews(nil, active)
+		return front.ServeTemplate(out, "index", views)
+	}
+
+	// Get the feed view for the current user
+	view, err := control.MakeFeedView(active)
+
+	if err != nil {
+		log.Printf("%s", err)
+	}
+
+	views := control.MakeViews(view, active)
 	return front.ServeTemplate(out, "index", views)
 }
