1fa75a4407a2f73ae6efdecc278600d377e76db1
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 22:17:32 -0400

    Add delete post handler

--- src/handlers/post.go
+++ src/handlers/post.go
@@ -34,3 +34,30 @@ func GetPost(out http.ResponseWriter, in *http.Request) *app.Error {
 
 	return front.ServeTemplate(out, "post", views)
 }
+
+func DeletePost(out http.ResponseWriter, in *http.Request) *app.Error {
+	// Extract the handle and timestamp from the URL
+	tokens := strings.Split(in.URL.Path, "/")
+	handle, stamp := tokens[2], tokens[4]
+
+	// Check if user exists
+	_, err := data.LoadUser(handle)
+
+	if err != nil {
+		return front.NotFound(err)
+	}
+
+	// Check if post exists
+	_, err = data.LoadPost(handle + "/" + stamp)
+
+	if err != nil {
+		return front.NotFound(err)
+	}
+
+	// Delete post and redirect
+	if err = data.RemovePost(handle + "/" + stamp); err != nil {
+		return front.ServerError(err)
+	}
+
+	return front.Redirect("/", nil, out, in)
+}
