c2800cc9939f691d29e8049f8528b5c7ca3cb48a
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-02 21:36:02 -0400

    Add post page handler

---
+++ src/handlers/post.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"../app"
+	"../control"
+	"../data"
+	"../front"
+	"net/http"
+	"strings"
+)
+
+func GetPost(out http.ResponseWriter, in *http.Request) *app.Error {
+	// Extract the handle and timestamp from the URL
+	tokens := strings.Split(in.URL.Path, "/")
+	handle, stamp := tokens[2], tokens[4]
+
+	// Check if user exists
+	_, err := data.LoadUser(handle)
+
+	if err != nil {
+		return front.NotFound(err)
+	}
+
+	// Check if post exists
+	post, err := data.LoadPost(handle + "/" + stamp)
+
+	if err != nil {
+		return front.NotFound(err)
+	}
+
+	// Get active user and build views
+	view := control.MakePostView(post)
+	views := control.GetUserAndMakeViews(view, in)
+
+	return front.ServeTemplate(out, "post", views)
+}
