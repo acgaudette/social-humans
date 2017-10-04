149fcf7abfec35d8b2abf8ef3970638ad7a24102
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-04 18:41:16 -0400

    Add Container struct methods

--- src/views/container.go
+++ src/views/container.go
@@ -1,3 +1,19 @@
 package views
 
 type Container map[string]View
+
+func (this Container) SetActive(active Active) {
+	this["active"] = active
+}
+
+func (this Container) SetContent(content View) {
+	this["content"] = content
+}
+
+func (this Container) SetStatus(status Status) {
+	this["status"] = status
+}
+
+func NewContainer() Container{
+	return make(Container)
+}
