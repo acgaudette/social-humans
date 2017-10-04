e9925e76f257e9d12a9ebc90fe8e84daf59fd3be
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-04 13:27:38 -0400

    Add controller errors file

---
+++ src/control/errors.go
@@ -0,0 +1,28 @@
+package control
+
+import "fmt"
+
+type UserNotFoundError struct {
+	handle string
+}
+
+func (this *UserNotFoundError) Error() string {
+	return fmt.Sprintf("user \"%s\" not found", this.handle)
+}
+
+type EmptyFeedError struct {
+	handle string
+}
+
+func (this EmptyFeedError) Error() string {
+	return fmt.Sprintf("feed for user \"%s\" is empty", this.handle)
+}
+
+type AccessError struct {
+	handle string
+}
+
+func (this AccessError) Error() string {
+	return fmt.Sprintf("feed for user \"%s\" access failure", this.handle)
+}
+
