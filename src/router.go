f4cb14ab24288bf82a1a186ddeb44e3ec09425bd
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-05 04:21:08 -0400

    Comment router

--- src/router.go
+++ src/router.go
@@ -12,26 +12,32 @@ type Router struct {
 	routes *node
 }
 
+// Create new router with index handler
 func NewRouter(index app.Handler) *Router {
 	handlers := make(methodHandlers)
 	handlers[http.MethodGet] = index
 
+	// Initialize trie
 	routes := &node{
 		split:    "",
 		children: []*node{},
 		handlers: handlers,
 	}
 
-	return &Router{routes: routes}
+	return &Router{routes}
 }
 
+// Satisfies multiplexer interface
 func (this *Router) ServeHTTP(out http.ResponseWriter, in *http.Request) {
+	// Get path tokens
 	tokens := strings.Split(in.URL.Path, "/")
 
+	// Special case for root path
 	if tokens[1] == "" {
 		tokens = tokens[1:]
 	}
 
+	// Execute handler for route
 	err := this.routes.eval(tokens, out, in)
 
 	if err != nil {
@@ -39,18 +45,22 @@ func (this *Router) ServeHTTP(out http.ResponseWriter, in *http.Request) {
 	}
 }
 
-// Store a route in the router
+// Store a route (path to method handler) in the router
 func (this *Router) Handle(method, route string, handler app.Handler) error {
+	// Invalid route
 	if route[0] != '/' {
 		return fmt.Errorf("invalid route \"%s\"", route)
 	}
 
+	// Get path tokens
 	tokens := strings.Split(route, "/")
 
+	// Special case for root path
 	if tokens[1] == "" {
 		tokens = tokens[1:]
 	}
 
+	// Add new route
 	this.routes.append(method, tokens, handler)
 	return nil
 }
@@ -65,6 +75,8 @@ func (this *Router) POST(route string, handler app.Handler) error {
 	return this.Handle(http.MethodPost, route, handler)
 }
 
+/* Route trie implementation */
+
 type methodHandlers map[string]app.Handler
 
 type node struct {
@@ -73,41 +85,52 @@ type node struct {
 	handlers methodHandlers
 }
 
+// Append a handler to the trie
 func (this *node) append(method string, tokens []string, handler app.Handler) {
+	// Pop the first token
 	tokens = tokens[1:]
 
+	// Base case
 	if len(tokens) == 0 {
+		// Add first handler
 		if this.handlers == nil {
 			this.handlers = make(methodHandlers)
 		}
 
+		// Otherwise, add handler for new method
 		this.handlers[method] = handler
 		return
 	}
 
 	token := tokens[0]
 
+	// If a child exists with the token, call recursively
 	if child := this.get(token); child != nil {
 		child.append(method, tokens, handler)
 		return
 	}
 
+	// Otherwise, create new node
 	next := &node{
 		split:    token,
 		children: []*node{},
 		handlers: nil,
 	}
 
+	// Add child and call recursively
 	this.add(next)
 	next.append(method, tokens, handler)
 }
 
+// Recursively look for and execute a handler
 func (this *node) eval(
 	tokens []string, out http.ResponseWriter, in *http.Request,
 ) error {
 	tokens = tokens[1:]
 
+	// Base case
 	if len(tokens) == 0 {
+		// If handler exists for method, execute
 		if handler, ok := this.handlers[in.Method]; ok {
 			app.Handle(handler, out, in)
 			return nil
@@ -125,20 +148,26 @@ func (this *node) eval(
 
 	token := tokens[0]
 
+	// If a child exists with the token, call recursively
 	if child := this.get(token); child != nil {
 		return child.eval(tokens, out, in)
 	}
 
+	// Otherwise, error
 	http.NotFound(out, in)
 	return fmt.Errorf("route not found for \"%s\"", in.URL.Path)
 }
 
+// Add child
 func (this *node) add(next *node) {
 	this.children = append(this.children, next)
 }
 
+// Get child with matching split
 func (this *node) get(split string) *node {
+	// Iterate through node children
 	for _, child := range this.children {
+		// Match exact or wildcard
 		if child.split == split || child.split == "*" {
 			return child
 		}
