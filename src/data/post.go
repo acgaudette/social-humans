7fb0755682bd9d16a3db100cee7940e757b1270f
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-06 04:45:25 -0400

    Change name and parameter of AddPost

--- src/data/post.go
+++ src/data/post.go
@@ -10,6 +10,7 @@ import (
 	"time"
 )
 
+// Post data representation structure
 type Post struct {
 	Title     string
 	Content   string
@@ -17,76 +18,50 @@ type Post struct {
 	Timestamp string
 }
 
+// Post data wrapper for serialization
 type postData struct {
 	Title   string
 	Content string
 }
 
-func (this *Post) MarshalBinary() ([]byte, error) {
-	wrapper := &postData{
-		Title:   this.Title,
-		Content: this.Content,
-	}
-
-	var buffer bytes.Buffer
-	encoder := gob.NewEncoder(&buffer)
-
-	if err := encoder.Encode(wrapper); err != nil {
-		return nil, err
-	}
-
-	return buffer.Bytes(), nil
-}
-
-func (this *Post) UnmarshalBinary(buffer []byte) error {
-	wrapper := postData{}
-
-	reader := bytes.NewReader(buffer)
-	decoder := gob.NewDecoder(reader)
-
-	if err := decoder.Decode(&wrapper); err != nil {
-		return err
-	}
-
-	this.Title = wrapper.Title
-	this.Content = wrapper.Content
-
-	return nil
-}
-
+// The address is a unique string identifier for the post
 func (this *Post) GetAddress() string {
 	return this.Author + "/" + this.Timestamp
 }
 
 // Write post to file
 func (this *Post) save() error {
+	// Serialize
 	buffer, err := this.MarshalBinary()
 
 	if err != nil {
 		return err
 	}
 
+	// Create user directory if it doesn't already exist
 	dir := prefix(this.Author + "/")
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		os.Mkdir(dir, os.ModePerm)
 	}
 
+	// Write to file
 	return ioutil.WriteFile(
 		prefix(this.GetAddress()+".post"), buffer, 0600,
 	)
 }
 
 // Create new post and save
-func NewPost(title, content, author string) error {
+func AddPost(title, content string, author *User) error {
 	stamp := time.Now().UTC().Format(TIMESTAMP_LAYOUT)
 
 	this := &Post{
 		Title:     title,
 		Content:   content,
-		Author:    author,
+		Author:    author.Handle,
 		Timestamp: stamp,
 	}
 
+	// Update data
 	if err := this.save(); err != nil {
 		return err
 	}
@@ -96,7 +71,7 @@ func NewPost(title, content, author string) error {
 	return nil
 }
 
-// Return titles of all post files for a given user
+// Return addresses of all post files for a given user handle
 func GetPostAddresses(author string) ([]string, error) {
 	// Read posts directory for user
 	files, err := ioutil.ReadDir(prefix(author + "/"))
@@ -107,6 +82,7 @@ func GetPostAddresses(author string) ([]string, error) {
 
 	addresses := []string{}
 
+	// Build addresses slice
 	for _, file := range files {
 		// Get address from filename
 		address := author + "/" + strings.Split(file.Name(), ".")[0]
@@ -116,7 +92,7 @@ func GetPostAddresses(author string) ([]string, error) {
 	return addresses, nil
 }
 
-// Where the address is "author/timestamp"
+// Load post data with lookup address
 func LoadPost(address string) (*Post, error) {
 	// Read post file
 	buffer, err := ioutil.ReadFile(prefix(address + ".post"))
@@ -134,6 +110,7 @@ func LoadPost(address string) (*Post, error) {
 		Timestamp: stamp,
 	}
 
+	// Deserialize
 	err = loaded.UnmarshalBinary(buffer)
 
 	if err != nil {
@@ -153,21 +130,61 @@ func UpdatePost(title, content, author, stamp string) error {
 		Timestamp: stamp,
 	}
 
+	// Update data
 	if err := this.save(); err != nil {
 		return err
 	}
 
-	log.Printf("Updated post id \"%s\" titled \"\"%s\" by \"%s\"", stamp, title, author)
+	log.Printf("Updated post \"%s\" by \"%s\"", title, author)
 
 	return nil
 }
 
+// Remove post with lookup address
 func RemovePost(address string) error {
 	if err := os.Remove(prefix(address + ".post")); err != nil {
 		return err
 	}
 
 	log.Printf("Deleted post \"%s\"", address)
+
+	return nil
+}
+
+/* Satisfy binary interfaces */
+
+func (this *Post) MarshalBinary() ([]byte, error) {
+	// Create wrapper from post struct
+	wrapper := &postData{
+		Title:   this.Title,
+		Content: this.Content,
+	}
+
+	var buffer bytes.Buffer
+	encoder := gob.NewEncoder(&buffer)
+
+	// Encode wrapper with gob
+	if err := encoder.Encode(wrapper); err != nil {
+		return nil, err
+	}
+
+	return buffer.Bytes(), nil
+}
+
+func (this *Post) UnmarshalBinary(buffer []byte) error {
+	wrapper := postData{}
+
+	reader := bytes.NewReader(buffer)
+	decoder := gob.NewDecoder(reader)
+
+	// Decode wrapper with gob
+	if err := decoder.Decode(&wrapper); err != nil {
+		return err
+	}
+
+	// Load wrapper into new user struct
+	this.Title = wrapper.Title
+	this.Content = wrapper.Content
 
 	return nil
 }
