90f6d4a9ac0b2283fb891823822f1dfa0ed5cce9
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-06 00:35:50 -0400

    Factor out post form code to controller

--- src/control/form.go
+++ src/control/form.go
@@ -1,11 +1,15 @@
 package control
 
 import (
+	"../app"
+	"../data"
 	"fmt"
 	"net/url"
 	"strings"
+	"unicode/utf8"
 )
 
+// Read string from form and check for invalid input
 func SanitizeFormString(key string, form *url.Values) (string, error) {
 	result := form.Get(key)
 
@@ -17,6 +21,7 @@ func SanitizeFormString(key string, form *url.Values) (string, error) {
 	return result, nil
 }
 
+// Read selection from radio
 func ReadFormRadio(
 	key string, options []string, form *url.Values,
 ) (string, error) {
@@ -27,4 +32,41 @@ func ReadFormRadio(
 	}
 
 	return "", fmt.Errorf("key \"%s\" not found for radio form")
+}
+
+// Helper function shared by create_post and edit_post
+func ReadPostForm(
+	serve func(string) *app.Error, form *url.Values,
+) (string, string, *app.Error) {
+	title := form.Get("title")
+
+	if title == "" {
+		return "", "", serve("Title required!")
+	}
+
+	// Check title character limit
+	if utf8.RuneCountInString(title) > data.TITLE_LIMIT {
+		return "", "", serve(
+			fmt.Sprintf(
+				"Post title must be under %v characters", data.TITLE_LIMIT,
+			),
+		)
+	}
+
+	content := form.Get("content")
+
+	if content == "" {
+		return "", "", serve("Post content required!")
+	}
+
+	// Check content character limit
+	if utf8.RuneCountInString(content) > data.CONTENT_LIMIT {
+		return "", "", serve(
+			fmt.Sprintf(
+				"Post content must be under %v characters", data.CONTENT_LIMIT,
+			),
+		)
+	}
+
+	return title, content, nil
 }
