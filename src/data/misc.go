90647566330a7f8f143c033974e340004292c384
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-09 15:31:38 -0400

    Move getCommitHash function to misc

--- src/data/misc.go
+++ src/data/misc.go
@@ -4,6 +4,8 @@ import (
 	"crypto/rand"
 	"crypto/sha256"
 	"fmt"
+	"io/ioutil"
+	"strings"
 )
 
 // Get data path prefix
@@ -23,4 +25,36 @@ func generateToken() string {
 	buffer := make([]byte, 32)
 	rand.Read(buffer)
 	return fmt.Sprintf("%x", buffer)
+}
+
+// Read the short commit hash from file
+func getCommitHash(root string) (*string, error) {
+	// Read HEAD
+	buffer, err := ioutil.ReadFile(root + "/HEAD")
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert to string and trim newline
+	path := string(buffer[:])
+	path = path[:len(path)-1]
+
+	// Get path from HEAD ref
+	ref := strings.Split(path, " ")[1]
+
+	// Read commit hash
+	buffer, err = ioutil.ReadFile(root + "/" + ref)
+
+	if err != nil {
+		return nil, err
+	}
+
+	// Convert to string
+	hash := string(buffer[:])
+
+	// Make short hash
+	hash = hash[:7]
+
+	return &hash, nil
 }
