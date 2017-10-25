39724c9b54664da5d420ca6a6013a915651b31f9
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-24 23:37:56 -0400

    Remove constants

--- src/data/constants.go
+++ src/data/constants.go
@@ -1,12 +1,9 @@
 package data
 
 const (
-	DATA_PATH         = "data"
-	SESSION_NAME      = "social-humans"
-	DELM              = "+"
-	TIMESTAMP_LAYOUT  = "20060102150405"
-	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
 	TITLE_LIMIT       = 20
 	CONTENT_LIMIT     = 100
 	DEFAULT_GIT_ROOT  = ".git"
+	TIMESTAMP_LAYOUT  = "20060102150405"
+	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
 )
