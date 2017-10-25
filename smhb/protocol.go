0b528ed569f7574af4c935fa26e5b5afa6e7afbe
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-25 01:10:59 -0400

    Implement header serialization

--- smhb/protocol.go
+++ smhb/protocol.go
@@ -1,9 +1,9 @@
 package smhb
 
-type Header struct {
-	request [2]byte
-	length  [2]byte
-}
+import (
+	"encoding/binary"
+	"net"
+)
 
 type PROTOCOL int
 
@@ -16,3 +16,34 @@ type REQUEST uint16
 const (
 	USER = REQUEST(0)
 )
+
+type header struct {
+	request REQUEST
+	length  uint16
+}
+
+func getHeader(connection net.Conn) (header, error) {
+	this := header{}
+
+	err := binary.Read(connection, binary.LittleEndian, &this.request)
+
+	if err != nil {
+		return this, err
+	}
+
+	err = binary.Read(connection, binary.LittleEndian, &this.length)
+
+	return this, nil
+}
+
+func setHeader(connection net.Conn, request REQUEST, length uint16) error {
+	err := binary.Write(connection, binary.LittleEndian, request)
+
+	if err != nil {
+		return err
+	}
+
+	err = binary.Write(connection, binary.LittleEndian, length)
+
+	return err
+}
