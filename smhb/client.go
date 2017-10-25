96ff69799a39213dfcee58724844d5f8f33dadd9
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-25 00:48:40 -0400

    Get response from server in client

--- smhb/client.go
+++ smhb/client.go
@@ -2,6 +2,8 @@ package smhb
 
 import (
 	"encoding/binary"
+	"errors"
+	"io"
 	"net"
 	"strconv"
 )
@@ -10,7 +12,16 @@ type Client interface {
 	ServerAddress() string
 	ServerPort() int
 	Protocol() PROTOCOL
-	Query(request REQUEST) error
+}
+
+func NewClient(
+	serverAddress string, serverPort int, protocol PROTOCOL,
+) Client {
+	return client{
+		serverAddress,
+		serverPort,
+		protocol,
+	}
 }
 
 type client struct {
@@ -31,40 +42,64 @@ func (this client) Protocol() PROTOCOL {
 	return this.protocol
 }
 
-func (this client) Query(request REQUEST) error {
+func (this client) query(request REQUEST) ([]byte, error) {
 	switch this.protocol {
 	case TCP:
 		bind := this.serverAddress + ":" + strconv.Itoa(this.serverPort)
 		connection, err := net.Dial("tcp", bind)
 
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		defer connection.Close()
 
+		// Request
+
 		err = binary.Write(connection, binary.LittleEndian, request)
 
 		if err != nil {
-			return err
+			return nil, err
 		}
 
-		err = binary.Write(connection, binary.LittleEndian, uint16(4)) // fix this later
+		err = binary.Write(connection, binary.LittleEndian, uint16(4))
 
 		if err != nil {
-			return err
+			return nil, err
 		}
+
+		// Response
+
+		var response, length uint16
+
+		err = binary.Read(connection, binary.LittleEndian, &request)
+
+		if err != nil {
+			return nil, err
+		}
+
+		err = binary.Read(connection, binary.LittleEndian, &length)
+
+		if err != nil {
+			return nil, err
+		}
+
+		// Validate
+
+		if REQUEST(response) != request {
+			return nil, errors.New("invalid response")
+		}
+
+		length -= 4
+		buffer := make([]byte, length)
+		_, err = io.ReadFull(connection, buffer)
+
+		if err != nil {
+			return nil, err
+		}
+
+		return buffer, nil
 	}
 
-	return nil
-}
-
-func NewClient(
-	serverAddress string, serverPort int, protocol PROTOCOL,
-) Client {
-	return client{
-		serverAddress,
-		serverPort,
-		protocol,
-	}
+	return nil, nil
 }
