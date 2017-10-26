e504fd1e30ce84d13565a1bc995a76b8249fc1cb
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-25 20:06:10 -0400

    Add post delete method to client

--- smhb/deletes.go
+++ smhb/deletes.go
@@ -3,3 +3,7 @@ package smhb
 func (this client) DeleteUser(handle string) error {
 	return this.delete(USER, handle)
 }
+
+func (this client) DeletePost(address string) error {
+	return this.delete(POST, address)
+}
