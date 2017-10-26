d5e6a2d087c997d284a162c651e9a640ec5e7fd4
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-25 23:45:06 -0400

    Add edit post method to client implementation

--- smhb/edits.go
+++ smhb/edits.go
@@ -7,3 +7,18 @@ func (this client) EditPoolAdd(owner, handle string) error {
 func (this client) EditPoolBlock(owner, handle string) error {
 	return this.edit(POOL_BLOCK, owner, []byte(handle))
 }
+
+type postEdit struct {
+	Title string
+	Content string
+}
+
+func (this client) EditPost(address, title, content string) error {
+	data, err := serialize(postEdit{title, content})
+
+	if err != nil {
+		return err
+	}
+
+	return this.edit(POST, address, data)
+}
