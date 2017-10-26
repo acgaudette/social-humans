e070696c9ee9d9dd989b8535a7f0003b7e279056
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-26 00:33:35 -0400

    Split constants between frontend and backend

--- src/data/constants.go
+++ src/data/constants.go
@@ -3,8 +3,11 @@ package data
 const (
 	BACKEND_ADDRESS   = "0.0.0.0"
 	BACKEND_PORT      = 19138
+	DATA_PATH         = "sessions"
+	SESSION_NAME      = "social-humans"
+	DELM              = "+"
 	TITLE_LIMIT       = 20
 	CONTENT_LIMIT     = 100
-	DEFAULT_GIT_ROOT  = ".git"
 	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
+	DEFAULT_GIT_ROOT  = ".git"
 )
