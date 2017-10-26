45ed43f544b3474c92e0f36f52e4eee799114424
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-26 00:34:03 -0400

    Move post address build function to misc

--- smhb/misc.go
+++ smhb/misc.go
@@ -2,10 +2,8 @@ package smhb
 
 import (
 	"bytes"
-	"crypto/rand"
 	"crypto/sha256"
 	"encoding/gob"
-	"fmt"
 )
 
 // Get data path prefix
@@ -13,18 +11,16 @@ func prefix(extension string) string {
 	return DATA_PATH + "/" + extension
 }
 
+// The address is a unique string identifier for the post
+func BuildPostAddress(handle, stamp string) string {
+	return handle + "/" + stamp
+}
+
 // Hash password
 func hash(cleartext string) []byte {
 	hash := sha256.New()
 	hash.Write([]byte(cleartext))
 	return hash.Sum(nil)
-}
-
-// Generate random string
-func generateToken() string {
-	buffer := make([]byte, 32)
-	rand.Read(buffer)
-	return fmt.Sprintf("%x", buffer)
 }
 
 func serialize(this interface{}) ([]byte, error) {
