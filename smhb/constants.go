3a9438477b19f3092fb0c7d71a2cb29093fd5bf1
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-26 00:33:35 -0400

    Split constants between frontend and backend

--- smhb/constants.go
+++ smhb/constants.go
@@ -2,8 +2,6 @@ package smhb
 
 const (
 	DATA_PATH        = "data"
-	SESSION_NAME     = "social-humans"
-	DELM             = "+"
 	TIMESTAMP_LAYOUT = "20060102150405"
 	TARGET_LENGTH    = 26
 	WORKER_COUNT     = 8
