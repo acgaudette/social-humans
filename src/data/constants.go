9612d3043a372a2caaa4c7ed7b16223b033c5dbb
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-26 17:32:27 -0400

    Add handle limit constant to data package

--- src/data/constants.go
+++ src/data/constants.go
@@ -1,5 +1,9 @@
 package data
 
+import (
+	"../../smhb"
+)
+
 const (
 	BACKEND_ADDRESS   = "0.0.0.0"
 	BACKEND_PORT      = 19138
@@ -8,6 +12,7 @@ const (
 	DELM              = "+"
 	TITLE_LIMIT       = 20
 	CONTENT_LIMIT     = 100
+	HANDLE_LIMIT      = smhb.TARGET_LENGTH
 	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
 	DEFAULT_GIT_ROOT  = ".git"
 )
