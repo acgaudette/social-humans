df1525f0acb4381839e300fd9cbdf7bd5b295794
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-29 07:55:53 -0400

    Add TOKEN_SIZE to frontend constants

--- src/data/constants.go
+++ src/data/constants.go
@@ -6,6 +6,7 @@ const (
 	DATA_PATH         = "sessions"
 	SESSION_NAME      = "social-humans"
 	DELM              = "+"
+	TOKEN_SIZE        = 32
 	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
 	DEFAULT_GIT_ROOT  = ".git"
 )
