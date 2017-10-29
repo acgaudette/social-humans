ef8007fdb02d2781b49ce33f24cb62a33bcbc0d1
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-28 20:21:54 -0400

    Comment smhb constants

--- smhb/constants.go
+++ smhb/constants.go
@@ -1,13 +1,30 @@
 package smhb
 
 const (
-	QUEUE_SIZE       = 128
-	IO_TIMEOUT       = 10
+	/* Work channel buffer size */
+
+	QUEUE_SIZE = 128
+
+	/* Timeout before closing connection (seconds) */
+
+	IO_TIMEOUT = 10
+
+	/* Post address identifier format */
+
 	TIMESTAMP_LAYOUT = "20060102150405"
-	ADDRESS_LENGTH   = len(TIMESTAMP_LAYOUT) + 1
-	HEADER_SIZE      = 64
-	TARGET_LENGTH    = HEADER_SIZE - 6 - 1
-	HANDLE_LIMIT     = TARGET_LENGTH - ADDRESS_LENGTH
-	TITLE_LIMIT      = 20
-	CONTENT_LIMIT    = 100
+	ADDRESS_LENGTH   = len(TIMESTAMP_LAYOUT) + 1 // Includes the leading slash
+
+	/* Length of message header */
+
+	HEADER_SIZE = 64
+
+	/* Size of target string in header */
+
+	TARGET_LENGTH = HEADER_SIZE - 6 - 1 // Includes the null-terminator byte
+
+	/* Content character limits */
+
+	HANDLE_LIMIT  = TARGET_LENGTH - ADDRESS_LENGTH
+	TITLE_LIMIT   = 20
+	CONTENT_LIMIT = 100
 )
