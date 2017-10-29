85874c760df295575279fa14ad4a0844db443a41
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-29 04:20:38 -0400

    Get and return token in store user request

--- smhb/stores.go
+++ smhb/stores.go
@@ -6,20 +6,34 @@ type userStore struct {
 	Name     string
 }
 
-func (this client) AddUser(handle, password, name string) (User, error) {
+func (this client) AddUser(
+	handle, password, name string,
+) (User, *Token, error) {
 	data, err := serialize(userStore{password, name})
 
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	err = this.store(USER, handle, data)
+	err = this.store(USER, handle, data, nil)
 
 	if err != nil {
-		return nil, err
+		return nil, nil, err
 	}
 
-	return this.GetUser(handle)
+	token, err := this.GetToken(handle, password)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	loaded, err := this.GetUser(handle)
+
+	if err != nil {
+		return nil, nil, err
+	}
+
+	return loaded, token, nil
 }
 
 // Data to send along with a post store
@@ -28,12 +42,12 @@ type postStore struct {
 	Author  string
 }
 
-func (this client) AddPost(title, content, author string) error {
+func (this client) AddPost(title, content, author string, token Token) error {
 	data, err := serialize(postStore{content, author})
 
 	if err != nil {
 		return err
 	}
 
-	return this.store(POST, title, data)
+	return this.store(POST, title, data, &token)
 }
