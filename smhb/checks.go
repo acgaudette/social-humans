a3396e02bf38b6934f2a9bab6c21a9c47bc10c35
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-29 07:56:38 -0400

    Add client token check method implementation

--- smhb/checks.go
+++ smhb/checks.go
@@ -4,6 +4,10 @@ func (this client) Validate(handle, cleartext string) error {
 	return this.check(VALIDATE, handle, []byte(cleartext))
 }
 
+func (this client) CheckToken(handle string) error {
+	return this.check(TOKEN, handle, nil)
+}
+
 func (this client) CheckUser(handle string) error {
 	return this.check(USER, handle, nil)
 }
