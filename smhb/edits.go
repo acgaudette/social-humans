923196df74d508c6d6d2d369bbe5ad78e5007622
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-29 04:22:26 -0400

    Update edit client methods to support tokens

--- smhb/edits.go
+++ smhb/edits.go
@@ -1,21 +1,11 @@
 package smhb
 
-func (this client) Validate(handle, cleartext string) (bool, error) {
-	err := this.edit(VALIDATE, handle, []byte(cleartext))
-
-	if err != nil {
-		return false, err
-	}
-
-	return true, nil
+func (this client) EditPoolAdd(owner, handle string, token Token) error {
+	return this.edit(POOL_ADD, owner, []byte(handle), &token)
 }
 
-func (this client) EditPoolAdd(owner, handle string) error {
-	return this.edit(POOL_ADD, owner, []byte(handle))
-}
-
-func (this client) EditPoolBlock(owner, handle string) error {
-	return this.edit(POOL_BLOCK, owner, []byte(handle))
+func (this client) EditPoolBlock(owner, handle string, token Token) error {
+	return this.edit(POOL_BLOCK, owner, []byte(handle), &token)
 }
 
 // Data to send along with a post edit
@@ -24,20 +14,24 @@ type postEdit struct {
 	Content string
 }
 
-func (this client) EditPost(address, title, content string) error {
+func (this client) EditPost(
+	address, title, content string, token Token,
+) error {
 	data, err := serialize(postEdit{title, content})
 
 	if err != nil {
 		return err
 	}
 
-	return this.edit(POST, address, data)
+	return this.edit(POST, address, data, &token)
 }
 
-func (this client) EditUserName(handle, name string) error {
-	return this.edit(USER_NAME, handle, []byte(name))
+func (this client) EditUserName(handle, name string, token Token) error {
+	return this.edit(USER_NAME, handle, []byte(name), &token)
 }
 
-func (this client) EditUserPassword(handle, password string) error {
-	return this.edit(USER_PASSWORD, handle, []byte(password))
+func (this client) EditUserPassword(
+	handle, password string, token Token,
+) error {
+	return this.edit(USER_PASSWORD, handle, []byte(password), &token)
 }
