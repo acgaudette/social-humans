262690fbe0e97d9b0d964011b0fc226fe02f6806
Aaron C Gaudette <gaudetteaaron@gmail.com>
2017-10-28 20:10:18 -0400

    Remove redundant constant from data package

--- src/data/constants.go
+++ src/data/constants.go
@@ -1,16 +1,11 @@
 package data
 
-import (
-	"../../smhb"
-)
-
 const (
 	BACKEND_ADDRESS   = "0.0.0.0"
 	BACKEND_PORT      = 19138
 	DATA_PATH         = "sessions"
 	SESSION_NAME      = "social-humans"
 	DELM              = "+"
-	HANDLE_LIMIT      = smhb.TARGET_LENGTH
 	HUMAN_TIME_LAYOUT = "3:04 PM, 2 Jan 2006"
 	DEFAULT_GIT_ROOT  = ".git"
 )
